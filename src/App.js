a5e7dea15ca098be484b04f16f32c0e2332fc13a
freelaeder <yanglo67@qq.com>
2023-03-14 20:38:49 +0800

    initial project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div className={'active1'}>
+          <h1 className="text-3xl font-bold underline text-blue-900">
+              Hello world!
+          </h1>
+          <p className={'text-6xl inline-block bg-yellow-100' }>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam cumque, debitis, esse exercitationem facere fugiat harum itaque nobis nostrum optio placeat quidem reiciendis sequi tempora ut vel voluptates? Debitis.</p>
+      </div>
 
-export default App;
+  )
+}
